e9c694f5797d6d52ff5bf228fcd8786ebf929e1a
CarCedo <CarCedo.Pro@gmail.com>
2023-07-22 22:12:07 +0330

    Added code for errors

--- URL Shortner/main.py
+++ URL Shortner/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,12 @@ def home_page():
 
 @app.route("/submit_url", methods=["POST"])
 def submit_url():
-	return render_template("success.html")
+	submitted_url = request.form.get("url")
+	if submitted_url:
+		
+		return render_template("success.html")
+	else:
+		return render_template("error.html", error_message="Invalid request payload, please try again.")
 
 if __name__ == "__main__":
 	app.run(debug = True)
