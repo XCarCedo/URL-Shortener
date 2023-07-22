dce0f78dc971bcf44a42f577dd1f44e54abd9eae
CarCedo <CarCedo.Pro@gmail.com>
2023-07-22 22:10:41 +0330

    Small change

--- URL Shortner/utils.py
+++ URL Shortner/utils.py
@@ -4,5 +4,3 @@ import string
 def generate_id(chars: str = string.ascii_lowercase + string.ascii_uppercase + string.digits,
 	lenght: int = 10):
 	return "".join([random.choice(chars) for _ in range(lenght)])
-
-print(generate_id())
