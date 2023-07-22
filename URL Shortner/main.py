213ef72f993a93f1cc87c68dfd3449c27dd7212a
CarCedo <CarCedo.Pro@gmail.com>
2023-07-22 23:26:13 +0330

    Added redirect route for the shortend urls

--- URL Shortner/main.py
+++ URL Shortner/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from models import db, URL
 from utils import generate_id
 
@@ -11,7 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE}"
 
 db.init_app(app)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home_page():
 	return render_template("home.html")
 
@@ -27,6 +27,15 @@ def submit_url():
 		return render_template("success.html", shortend_url=f"{request.host_url}redirect?id={link_id}")
 	else:
 		return render_template("error.html", error_message="Invalid request payload, please try again.")
+
+@app.route("/redirect", methods=["GET"])
+def redirect_page():
+	_id = request.args.get("id")
+	if _id:
+		requested_url = URL.query.get_or_404(_id)
+		return redirect(requested_url.url)
+	else:
+		return render_template("error.html", error_message="Invalid url, check your link.")
 
 if __name__ == "__main__":
 	if not Path(f"instance/{DB_FILE}").exists():
